84586d645bf35378b84e9b5ca4890236c67a7c58
George Varghese <geoji2000@gmail.com>
2019-09-21 11:24:07 -0500

    Embed jetty to provide a built-in webserver

---
+++ src/main/java/org/geoji/app/speaker/HelloServlet.java
@@ -0,0 +1,19 @@
+package org.geoji.app.speaker;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class HelloServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("<h1>Double Hello from HelloServlet</h1>");
+    }
+}
